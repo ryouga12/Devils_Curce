6a69bd93f32d05df1e51acc501784222db50032e
ryouga12 <ryouga.20011108@gmail.com>
2024-07-23 13:04:10 +0900

    [修正]NPCのメンバ変数の見直し

--- ManagedDxlGame2023f/ManagedDxlGame/program/game/Object/NPC.cpp
+++ ManagedDxlGame2023f/ManagedDxlGame/program/game/Object/NPC.cpp
@@ -1,26 +1,31 @@
 #include "NPC.h"
+#include"../Manager/ResourceManager.h"
+#include"../Manager/GameManager.h"
+#include"../Manager/UiManager.h"
 
-//Npcのコンストラクタ : 引数 : ( 名前 , 座標 ,キャラのサイズ , ハンドルのPass , 画像の大きさ , どのアニメーションから始めるか, ハンドルの数 )
-Npc::Npc(const std::string& name, const tnl::Vector3& pos, const int CHARASIZE, std::string ghdl_pass[], float scale , int animation_namber , int ghdl_num)
+
+//Npcのコンストラクタ : 引数 : ( 名前 , 座標 ,キャラのサイズ , ハンドルのPass , 画像の大きさ , どのアニメーションから始めるか, ハンドルの数 , アイコン )
+Npc::Npc(const std::string& name, const tnl::Vector3& pos, const int CHARASIZE, std::string ghdl_pass[], float scale , const NPC_ANIMATION animation_namber , const int ghdl_num , const std::string& npc_icon_image)
 {
 	//引数を代入して呼び出し元で使う
 	npc_name_ = name;
 	npc_pos_ = pos;
 	scale_ = scale;
-	npc_chara_chip_height = CHARASIZE;
-	npc_chara_chip_width = CHARASIZE;
-	animation_select = animation_namber;
+	npc_chara_chip.y = CHARASIZE;
+	npc_chara_chip.x = CHARASIZE;
+	anim_ctrl_dir = animation_namber;
+	npc_icon_hdl = ResourceManager::getResourceManager()->LoadGraphEX(npc_icon_image.c_str());
 	
 	for (int i = 0; i < ghdl_num; i++)
 	{
-		LoadDivGraph(ghdl_pass[i].c_str(), 3, 3, 1, npc_chara_chip_width, npc_chara_chip_height, anim_hdls[i]);
+		LoadDivGraph(ghdl_pass[i].c_str(), 3, 3, 1, npc_chara_chip.x, npc_chara_chip.y, anim_hdls[i]);
 	}
 }
 
 Npc::~Npc()
 {
 	//Npcの画像解放
-	for (int i = 0; i < static_cast<int>(NPC_DIR_MAX); i++) {
+	for (int i = 0; i < static_cast<int>(NPC_ANIMATION::NPC_DIR_MAX); i++) {
 		for (int k = 0; k < 5; k++) {
 			DeleteGraph(anim_hdls[i][k]);
 			// 解放後のポインタの値をデバッグトレースで確認
@@ -30,52 +35,14 @@ Npc::~Npc()
 
 }
 
-//Npcのアニメーションを切り替える
-void Npc::ChengeAnimation()
-{
-	switch (animation_select) {
-
-	case NPC_DIR_DOWN:
-
-		anim_ctrl_dir = NPC_DIR_DOWN;
-
-		break;
-
-	case NPC_DIR_UP:
-
-		anim_ctrl_dir = NPC_DIR_UP;
-
-		break;
-
-	case NPC_DIR_LEFT:
-
-		anim_ctrl_dir = NPC_DIR_LEFT;
-
-		break;
-
-	case NPC_DIR_RIGHT:
-
-		anim_ctrl_dir = NPC_DIR_RIGHT;
-
-		break;
-
-	default:
-
-		anim_ctrl_dir = NPC_DIR_DOWN;
-
-		break;
-
-	}
-}
-
 //更新処理
 void Npc::Update(float delta_time)
 {
 	//アニメーション
 	anim_time_count += delta_time;
-	if (anim_time_count > CHANGEANIMATIONTIME) {
+	if (anim_time_count > ChangeAnimationTime) {
 		anim_ctrl_frame++;
-		anim_ctrl_frame %= ANIMATIONFLAME;
+		anim_ctrl_frame %= animationFlame;
 
 		anim_time_count = 0;
 	}
@@ -86,5 +53,13 @@ void Npc::Update(float delta_time)
 void Npc::Draw(const KonCamera& camera)
 {
 	tnl::Vector3 draw_pos = npc_pos_ - camera.getTargetPos() + tnl::Vector3(DXE_WINDOW_WIDTH >> 1, DXE_WINDOW_HEIGHT >> 1, 0);
-	DrawRotaGraph(static_cast<int>(draw_pos.x), static_cast<int>(draw_pos.y), scale_, COLOR_BRACK, anim_hdls[anim_ctrl_dir][anim_ctrl_frame], true);
+	DrawRotaGraph(static_cast<int>(draw_pos.x), static_cast<int>(draw_pos.y), scale_, COLOR_BRACK, anim_hdls[static_cast<int>(anim_ctrl_dir)][anim_ctrl_frame], true);
 }
+
+//---------------------------------------------------------------------------------------------------------
+//セーブを行うNPC
+
+
+
+
+
--- ManagedDxlGame2023f/ManagedDxlGame/program/game/Object/NPC.h
+++ ManagedDxlGame2023f/ManagedDxlGame/program/game/Object/NPC.h
@@ -3,94 +3,113 @@
 ///NPCクラス
 ///
 
-//NPCのアニメーション
-enum  {
-	//下
-	NPC_DIR_DOWN,
-	//上,
-	NPC_DIR_UP,
-	//左
-	NPC_DIR_LEFT,
-	//右
-	NPC_DIR_RIGHT,
-	//総数
-	NPC_DIR_MAX
-};
-
 
 #include "../../dxlib_ext/dxlib_ext.h"
-#include"../Manager/Camera.h"
+#include"../System/Camera.h"
 #include"Actor.h"
+#include"../Menu/MenuWindow.h"
+#include<unordered_map>
+
+class MenuWindow;
+class GameManager;
+class UIManager;
+
 
 class Npc  : public Actor{
 public:
 
+	//NPCのアニメーション
+	enum  class NPC_ANIMATION {
+		//左
+		NPC_DIR_LEFT,
+		//右
+		NPC_DIR_RIGHT,
+		//上,
+		NPC_DIR_UP,
+		//下
+		NPC_DIR_DOWN,
+		//総数
+		NPC_DIR_MAX
+	};
+
 	//Npcのコンストラクタ : 引数 : ( 名前 , 座標 , キャラのサイズ ,ハンドルのPass , 画像の大きさ , どのアニメーションから始めるか, ハンドルの数 )
-	Npc(const std::string& name, const tnl::Vector3& pos ,const int CHARASIZE, std::string ghdl_pass[], float scale , int animation_namber , int ghdl_num);
+	Npc(const std::string& name, const tnl::Vector3& pos ,const int CHARASIZE, std::string ghdl_pass[],float scale , const NPC_ANIMATION animation_namber , const int ghdl_num, const std::string& npc_icon_image);
 	~Npc();
 
-	void ChengeAnimation();
-
 	//更新処理
-	void Update(float delta_time)override;
+	virtual void Update(float delta_time)override;
 
 	//描画
-	void Draw(const KonCamera& camera)override;
+	virtual void Draw(const KonCamera& camera)override;
 
 	//Npcのキャラチップの幅と高さを取得する
 	int GetNpcSizeWidth()const {
-		return npc_chara_chip_width;
+		return npc_chara_chip.x;
 	}
 
 	int GetNpcSizeHegiht()const {
-		return npc_chara_chip_height;
+		return npc_chara_chip.y;
+	}
+
+	//Npcのキャラのサイズを取得する
+	tnl::Vector2i GetNpcCharaChip()const {
+		return npc_chara_chip;
 	}
 
 	//Npcの座標を取得する
-	tnl::Vector3 GetNpcPos() { return npc_pos_; }
+	tnl::Vector3& GetNpcPos(){ return npc_pos_; }
 
 	//Npcの名前を取得する
 	const std::string& GetNpcName()const {
 		return npc_name_;
 	}
 
+	//Npcのアニメーションを取得する
+	NPC_ANIMATION GetNpcAnimation()const {
+		return anim_ctrl_dir;
+	}
+
+	//保管しているアニメーションをを取得する
+	NPC_ANIMATION GetNpcAnimation_Before()const {
+		return before_animation_save;
+	}
+
+	//変更する前のアニメーションを保管する
+	void SetNpcAnimationSave(NPC_ANIMATION save_animation) {
+		before_animation_save = save_animation;
+	}
+
+	//Npcのアイコンを取得する
+	int GetNpcIconImage()const {
+		return npc_icon_hdl;
+	}
 
 //------------------------------------------------------------------------------------------------------------------------
 //アニメーション
 
 private:
 
-	//Npcのアニメーションを切り替える
-	int animation_select = 0;			
-
 	//アニメーションのカウント
 	float anim_time_count = 0;						
 
 	//アニメーションの選択
-	int anim_ctrl_dir = NPC_DIR_DOWN;				
+	NPC_ANIMATION anim_ctrl_dir = NPC_ANIMATION::NPC_DIR_DOWN;
+
+	//一個前のアニメーションを保存しておく為の変数
+	//主にアニメーションを戻すために使用
+	NPC_ANIMATION before_animation_save;
 
 	//フレーム
 	int anim_ctrl_frame = 0;						
 
 	//アニメーションの配列
-	int anim_hdls[NPC_DIR_MAX][5];		
-
-	//アニメーションを切り替える秒数
-	const float CHANGEANIMATIONTIME = 0.1f;
-	//フレーム
-	const int ANIMATIONFLAME = 3;
+	int anim_hdls[static_cast<int>(NPC_ANIMATION::NPC_DIR_MAX)][5];
 
 
 public:
 
-	//アニメーションセレクトを取得する
-	inline int GetAnimationSelect() { return animation_select; }
-
-	//アニメーションをリセットする
-	void NpcAnimationReset() { animation_select = 0; }
-
 	//アニメーションをセットする
-	void SetNpcAnimation(int animationID) { animation_select = animationID; }
+	void SetNpcAnimation(NPC_ANIMATION new_animation) { anim_ctrl_dir = new_animation; }
 
 
 //------------------------------------------------------------------------------------------------------------------------
@@ -122,11 +141,8 @@ private:
 	//npcの名前
 	std::string npc_name_;
 
-	//Npcのキャラチップの幅
-	int npc_chara_chip_width = 0;
-
-	//Npcのキャラチップの高さ
-	int npc_chara_chip_height = 0;
+	//Npcのキャラチップのサイズ
+	tnl::Vector2i npc_chara_chip = {};
 
 	//それぞれの画像で使う大きさを決める
 	float scale_ = 0;
@@ -137,5 +153,8 @@ private:
 	//npcの座標
 	tnl::Vector3 npc_pos_ = {};
 
+	//npcのアイコン
+	int npc_icon_hdl = 0;
 
 };
+
