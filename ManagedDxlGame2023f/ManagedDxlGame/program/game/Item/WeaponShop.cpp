876043af497abc089fefe2d4b217c28105852d90
ryouga12 <ryouga.20011108@gmail.com>
2024-07-23 12:30:36 +0900

    [修正]武器屋の仕様変更

--- ManagedDxlGame2023f/ManagedDxlGame/program/game/Item/WeaponShop.cpp
+++ ManagedDxlGame2023f/ManagedDxlGame/program/game/Item/WeaponShop.cpp
@@ -2,18 +2,76 @@
 #include"../Manager/UiManager.h"
 #include"../Manager/GameManager.h"
 
-WeaponShop::WeaponShop()
+
+WeaponShop::WeaponShop(const InMapScene::InMapState& curent_inmap_scene)
 {
-	weapon_ = std::make_shared<Weapon>();
 	menuWindow = UIManager::getUIManager()->getMenu("menu_window");
 
+	item_ = std::make_shared<Item>();
+
+	//武器屋のラインナップを決める
+	WeaponShopInit(curent_inmap_scene);
 }
 
 WeaponShop::~WeaponShop()
 {
-	SoundManager::getSoundManager()->daleteSound("sound/SoundEffect/decision.mp3");
-	SoundManager::getSoundManager()->daleteSound("sound/SoundEffect/cousour_bgm.mp3");
 }
+
+void WeaponShop::WeaponShopInit(const InMapScene::InMapState& curent_inmap_scene)
+{
+	weponList.clear();
+	
+	switch (curent_inmap_scene)
+	{
+	case InMapScene::InMapState::VILLAGE:
+
+		//一個目の店
+
+		//木の棒
+		weponList.emplace_back(item_->GetItemById(6));
+		//木の剣
+		weponList.emplace_back(item_->GetItemById(4));
+		//木のハンマー
+		weponList.emplace_back(item_->GetItemById(5));
+		//革の盾
+		weponList.emplace_back(item_->GetItemById(2));
+		//ポーション
+		weponList.emplace_back(item_->GetItemById(3));
+
+		break;
+
+	case InMapScene::InMapState::TOWN:
+
+		//冒険者の刀
+		weponList.emplace_back(item_->GetItemById(10));
+		//銅の剣
+		weponList.emplace_back(item_->GetItemById(0));
+		//革の鎧
+		weponList.emplace_back(item_->GetItemById(1));
+		//冒険者の鎧
+		weponList.emplace_back(item_->GetItemById(13));
+		//ハイポーション
+		weponList.emplace_back(item_->GetItemById(8));
+
+		break;
+
+	//城
+	//今後追加予定
+	case InMapScene::InMapState::CASTLE:
+
+		break;
+
+	default:
+		break;
+	}
+
+
+	//値段が低い順にソート
+	std::sort(weponList.begin(), weponList.end(), [](const ItemBase& left, const ItemBase& right) {
+		return left.getItemPrice() < right.getItemPrice();
+		});
+}
+
 
 void WeaponShop::Update(float delta_time)
 {
@@ -22,29 +80,28 @@ void WeaponShop::Update(float delta_time)
 
 void WeaponShop::Draw()
 {
+
 	//メニューの表示
-	menuWindow.lock()->Menu_draw(50, 50, 350, 400);
+	menuWindow.lock()->Menu_draw(MENU_WINDOW_POS.x,MENU_WINDOW_POS.y, MENU_WINDOW_WIDTH, MENU_WINDOW_HEIGHT);
 
-	//武器屋関連の表示
-	DrawString(60, 520, "武器屋", -1);
+	DrawString(WEAPON_SHOP_STRING_POS.x, WEAPON_SHOP_STRING_POS.y, "===== 武器屋 =====", COLOR_WHITE);
 
-	DrawString(110, 70, "===== 武器屋 =====", -1);
-
-	for (int i = 0; i < weapon_->GetWeaponValue().size(); ++i) {
-		DrawStringEx(85, 130 + i * 40, -1, "%d. %s \n(威力: %d, 防御力: %d,価格: %d)", i + 1, weapon_->GetWeaponValue()[i].getItemName().c_str(), weapon_->GetWeaponValue()[i].getItemDamage(), weapon_->GetWeaponValue()[i].getItemDefance(), weapon_->GetWeaponValue()[i].getItemPrice());
+	for (int i = 0; i < weponList.size(); ++i) {
+		DrawStringEx(WEAPON_CONNECTION_POS.x, WEAPON_CONNECTION_POS.y + i * SPACING, -1, "%d. %s \n(威力: %d, 防御力: %d,価格: %d)", i + 1, weponList[i].getItemName().c_str(), weponList[i].getItemDamage(), weponList[i].getItemDefance(), weponList[i].getItemPrice());
 	}
-	DrawString(85, 380, "購入したい武器の番号を\n選択してください(0: 終了)", -1);
+	DrawString(PROMPT_POS.x, PROMPT_POS.y, "購入したい武器の番号を\n選択してください(0: 終了)", COLOR_WHITE);
 
 
+	//武器商人のコメント
 	if (buyWeapon == Buy_Weapon::BUY) {
-		DrawString(150, 620, "「毎度あり!ありがとね！」", -1);
+		DrawString(MESSAGE_POS.x, MESSAGE_POS.y, "「毎度あり!ありがとね！」", COLOR_WHITE);
 	}
 	else if (buyWeapon == Buy_Weapon::NO) {
-		DrawString(150, 620, "「お金が足りないよ!出直してきな!」", -1);
+		DrawString(MESSAGE_POS.x, MESSAGE_POS.y, "「お金が足りないよ!出直してきな!」", COLOR_WHITE);
 	}
 
-	ResourceManager::getResourceManager()->DrawRotaGraphEx("graphics/cur_sannkaku2.png", 70, 140 + select_cousor * 40, 0.3f, 0, true);
-	DrawStringEx(100, 700, -1, "%d", weapon_->GetWeaponList().size());
+	ResourceManager::getResourceManager()->DrawRotaGraphEx("graphics/cur_sannkaku2.png", CURSOR_POS.x, CURSOR_POS.y + select_cousor * SPACING, CURSOR_SIZE, 0, true);
+	//DrawStringEx(100, 700, -1, "%d", weponList.size());
 
 
 }
@@ -53,19 +110,17 @@ void WeaponShop::Draw()
 void WeaponShop::WeaponShopCursorMove()
 {
 	if (tnl::Input::IsKeyDownTrigger(eKeys::KB_UP)) {
-		weapon_num = static_cast<int>(weapon_->GetWeaponList().size());
+		weapon_num = static_cast<int>(weponList.size());
 		SoundManager::getSoundManager()->sound_Play("sound/SoundEffect/cousour_bgm.mp3" , DX_PLAYTYPE_BACK);
 		select_cousor = (select_cousor + (weapon_num - 1)) % weapon_num;
 	}
 	else if (tnl::Input::IsKeyDownTrigger(eKeys::KB_DOWN)) {
-		weapon_num = static_cast<int>(weapon_->GetWeaponList().size());
+		weapon_num = static_cast<int>(weponList.size());
 		SoundManager::getSoundManager()->sound_Play("sound/SoundEffect/cousour_bgm.mp3", DX_PLAYTYPE_BACK);
 		select_cousor = (select_cousor + 1) % weapon_num;
 	}
-
-	
-
 }
+
 
 void WeaponShop::BuyWeapon(const Shared<Player>& player)
 {
@@ -73,12 +128,12 @@ void WeaponShop::BuyWeapon(const Shared<Player>& player)
 	auto& inventory = GameManager::getGameManager()->getInventory();
 
 	if (select_cousor == 0 && tnl::Input::IsKeyDownTrigger(eKeys::KB_RETURN)) {
-		if (inventory->GetInventoryList().size() == 20) return;
+		if (inventory->GetInventoryList().size() == INVENTORY_MAX_SIZE) return;
 		//決定音を鳴らす
 		SoundManager::getSoundManager()->sound_Play("sound/SoundEffect/decision.mp3", DX_PLAYTYPE_BACK);
-		if (player->getPlayerMoney() >= weapon_->GetWeaponValue()[0].getItemPrice()) {
-			player->ReducePlayerMoney(weapon_->GetWeaponValue()[0].getItemPrice());
-			inventory->AddInventory(weapon_->GetWeaponValue()[0].getItemId());
+		if (player->getPlayerMoney() >= weponList[0].getItemPrice()) {
+			player->ReducePlayerMoney(weponList[0].getItemPrice());
+			inventory->AddInventory(weponList[0].getItemId());
 			buyWeapon = Buy_Weapon::BUY;
 		}
 		else
@@ -88,12 +143,12 @@ void WeaponShop::BuyWeapon(const Shared<Player>& player)
 
 	}
 	if (select_cousor == 1 && tnl::Input::IsKeyDownTrigger(eKeys::KB_RETURN)) {
-		if (inventory->GetInventoryList().size() == 20) return;
+		if (inventory->GetInventoryList().size() == INVENTORY_MAX_SIZE) return;
 		//決定音を鳴らす
 		SoundManager::getSoundManager()->sound_Play("sound/SoundEffect/decision.mp3", DX_PLAYTYPE_BACK);
-		if (player->getPlayerMoney() >= weapon_->GetWeaponValue()[1].getItemPrice()) {
-			player->ReducePlayerMoney(weapon_->GetWeaponValue()[1].getItemPrice());
-			inventory->AddInventory(weapon_->GetWeaponValue()[1].getItemId());
+		if (player->getPlayerMoney() >= weponList[1].getItemPrice()) {
+			player->ReducePlayerMoney(weponList[1].getItemPrice());
+			inventory->AddInventory(weponList[1].getItemId());
 			buyWeapon = Buy_Weapon::BUY;
 		}
 		else {
@@ -102,12 +157,12 @@ void WeaponShop::BuyWeapon(const Shared<Player>& player)
 
 	}
 	if (select_cousor == 2 && tnl::Input::IsKeyDownTrigger(eKeys::KB_RETURN)) {
-		if (inventory->GetInventoryList().size() == 20) return;
+		if (inventory->GetInventoryList().size() == INVENTORY_MAX_SIZE) return;
 		//決定音を鳴らす
 		SoundManager::getSoundManager()->sound_Play("sound/SoundEffect/decision.mp3", DX_PLAYTYPE_BACK);
-		if (player->getPlayerMoney() >= weapon_->GetWeaponValue()[2].getItemPrice()) {
-			player->ReducePlayerMoney(weapon_->GetWeaponValue()[2].getItemPrice());
-			inventory->AddInventory(weapon_->GetWeaponValue()[2].getItemId());
+		if (player->getPlayerMoney() >= weponList[2].getItemPrice()) {
+			player->ReducePlayerMoney(weponList[2].getItemPrice());
+			inventory->AddInventory(weponList[2].getItemId());
 			buyWeapon = Buy_Weapon::BUY;
 		}
 		else {
@@ -115,12 +170,12 @@ void WeaponShop::BuyWeapon(const Shared<Player>& player)
 		}
 	}
 	if (select_cousor == 3 && tnl::Input::IsKeyDownTrigger(eKeys::KB_RETURN)) {
-		if (inventory->GetInventoryList().size() == 20) return;
+		if (inventory->GetInventoryList().size() == INVENTORY_MAX_SIZE) return;
 		//決定音を鳴らす
 		SoundManager::getSoundManager()->sound_Play("sound/SoundEffect/decision.mp3", DX_PLAYTYPE_BACK);
-		if (player->getPlayerMoney() >= weapon_->GetWeaponValue()[3].getItemPrice()) {
-			player->ReducePlayerMoney(weapon_->GetWeaponValue()[3].getItemPrice());
-			inventory->AddInventory(weapon_->GetWeaponValue()[3].getItemId());
+		if (player->getPlayerMoney() >= weponList[3].getItemPrice()) {
+			player->ReducePlayerMoney(weponList[3].getItemPrice());
+			inventory->AddInventory(weponList[3].getItemId());
 			buyWeapon = Buy_Weapon::BUY;
 		}
 		else {
@@ -128,12 +183,12 @@ void WeaponShop::BuyWeapon(const Shared<Player>& player)
 		}
 	}
 	if (select_cousor == 4 && tnl::Input::IsKeyDownTrigger(eKeys::KB_RETURN)) {
-		if (inventory->GetInventoryList().size() == 20) return;
+		if (inventory->GetInventoryList().size() == INVENTORY_MAX_SIZE) return;
 		//決定音を鳴らす
 		SoundManager::getSoundManager()->sound_Play("sound/SoundEffect/decision.mp3", DX_PLAYTYPE_BACK);
-		if (player->getPlayerMoney() >= weapon_->GetWeaponValue()[4].getItemPrice()) {
-			player->ReducePlayerMoney(weapon_->GetWeaponValue()[4].getItemPrice());
-			inventory->AddInventory(weapon_->GetWeaponValue()[4].getItemId());
+		if (player->getPlayerMoney() >= weponList[4].getItemPrice()) {
+			player->ReducePlayerMoney(weponList[4].getItemPrice());
+			inventory->AddInventory(weponList[4].getItemId());
 			buyWeapon = Buy_Weapon::BUY;
 		}
 		else {
--- ManagedDxlGame2023f/ManagedDxlGame/program/game/Item/WeaponShop.h
+++ ManagedDxlGame2023f/ManagedDxlGame/program/game/Item/WeaponShop.h
@@ -1,7 +1,8 @@
 #pragma once
 #include"../Object/Player.h"
-#include"Weapon.h"
 #include"Inventory.h"
+#include"Item.h"
+#include"../Scene/InMapScene.h"
 
 class UIManger;
 class GameManager;
@@ -9,24 +10,45 @@ class GameManager;
 class WeaponShop {
 public:
 
-	WeaponShop();
+	WeaponShop(const InMapScene::InMapState& curent_inmap_scene);
 	~WeaponShop();
 
 	void Update(float delta_time);
 
 	void Draw();
 
+	//武器の購入処理
+	//引数 : プレイヤーのポインタ
+	//武器屋で購入した際のインベントリへの追加とお金の処理
 	void BuyWeapon(const Shared<Player>& player);
 
+	//武器屋のカーソル処理
 	void WeaponShopCursorMove();
+
+	//武器屋の初期化処理
+	//引数 : InMapの状態
+	//引数で指定したマップの武器リストを初期化する
+	void WeaponShopInit(const InMapScene::InMapState& curent_inmap_scene);
+
+	//武器の値を他のクラスで使う
+	std::vector<ItemBase>& GetWeaponValue()
+	{
+		return weponList;
+	}
+
+	//武器リストの数を取得する
+	std::vector<ItemBase>& GetWeaponList() {
+		return weponList;
+	}
 	
 //------------------------------------------------------------------------------------------------------------------------
 //ポインタ
 private:
 
-	Shared<Weapon>weapon_ = nullptr;
+	//ウィンドウ
 	Shared<MenuWindow>use_equip = nullptr;
 	Weak<Menu>menuWindow;
+	Shared<Item>item_ = nullptr;
 
 //------------------------------------------------------------------------------------------------------------------------
 //武器関係
@@ -49,6 +71,47 @@ private:
 	int cousor_y = 0;
 
 	Buy_Weapon buyWeapon = Buy_Weapon::ENMPTY;
+
+	//武器を格納する配列
+	std::vector<ItemBase>weponList;
+
+	//白
+	const int COLOR_WHITE = -1;
+
+	//カーソルのサイズ
+	const float CURSOR_SIZE = 0.3;
+
+	//インベントリの最大サイズ数
+	const int INVENTORY_MAX_SIZE = 20;
+
+
+	//---座標系---//
+
+	//ウィンドウを表示する座標
+	tnl::Vector2i MENU_WINDOW_POS = { 50 ,50 };
+
+	//ウィンドウの幅
+	const int MENU_WINDOW_WIDTH = 350;
+	//ウィンドウの高さ
+	const int MENU_WINDOW_HEIGHT = 400;
+
+	//武器屋文字座標
+	const tnl::Vector2i WEAPON_SHOP_STRING_POS = { 110 , 70 };
+
+	//武器関連の文字の表示座標
+	const tnl::Vector2i WEAPON_CONNECTION_POS = { 85 , 130 };
+
+	//文字の間隔
+	const int SPACING = 40;
+
+	//購入時の選択文字の座標
+	const tnl::Vector2i PROMPT_POS = { 85 , 380 };
+
+	//武器商人のコメント文字座標
+	const tnl::Vector2i MESSAGE_POS = { 150 , 620 };
+
+	//カーソルの座標
+	const tnl::Vector2i CURSOR_POS = { 70 ,140 };
 
 public:
 
