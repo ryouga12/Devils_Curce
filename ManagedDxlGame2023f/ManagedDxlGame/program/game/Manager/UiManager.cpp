da8107eea30e61703b2997b15fadfde7bd731716
ryouga12 <ryouga.20011108@gmail.com>
2024-08-07 03:36:09 +0900

    [追加・修正]マジックナンバーの削除及びストーリー表記の関数の追加

--- ManagedDxlGame2023f/ManagedDxlGame/program/game/Manager/UiManager.cpp
+++ ManagedDxlGame2023f/ManagedDxlGame/program/game/Manager/UiManager.cpp
@@ -10,23 +10,7 @@ UIManager* UIManager::getUIManager()
 	return p_instance;
 }
 
-void UIManager::Update(float delta_time)
-{
-	//アイコン用のアニメーション
-	if (icon_animation) {
-		icon_animation->update(delta_time);
-	}
-}
-
-void UIManager::Draw()
-{
-	//アイコン用のアニメーションの描画
-	if (icon_animation) {
-		icon_animation->draw();
-	}
-}
-
-void UIManager::addMenu(const std::string& menuName, std::shared_ptr<Menu> menu)
+void UIManager::addMenu(const std::string& menuName, const std::shared_ptr<Menu>& menu)
 {
 	//メニューマップにあるかを確認して
 	auto it = menu_map.find(menuName);
@@ -59,23 +43,23 @@ void UIManager::Menu_Draw(const std::string& menuName, const int&  menu_x, const
 }
 
 //武器屋のコメント表示
-void UIManager::armsdealerComentDraw(int type)
+void UIManager::armsdealerComentDraw(const int& type)
 {
 	//武器屋に最初に話かけたときのコメント
 	if (type == ARMSDEALER_FIRST_COMET) {
-		DrawString(WEAPONSHOP.x, WEAPONSHOP.y, "武器屋", COLOR_WHITE);
-		DrawStringEx(ARMSDEALERCOMENT.x, ARMSDEALERCOMENT.y, COLOR_WHITE, "「 いらっしゃい！」");
-		DrawStringEx(WEAPONSHOP_BUY.x, WEAPONSHOP_BUY.y, -1, "(1 : 武器を購入する)");
-		DrawStringEx(MENU_CLOSE.x, MENU_CLOSE.y, COLOR_WHITE, "(0 : メニューを閉じる)");
+		DrawString(WEAPONSHOP.x, WEAPONSHOP.y, "武器屋", koni::Color::WHITE);
+		DrawStringEx(ARMSDEALERCOMENT.x, ARMSDEALERCOMENT.y, koni::Color::WHITE, "「 いらっしゃい！」");
+		DrawStringEx(WEAPONSHOP_BUY.x, WEAPONSHOP_BUY.y, koni::Color::WHITE, "(1 : 武器を購入する)");
+		DrawStringEx(MENU_CLOSE.x, MENU_CLOSE.y, koni::Color::WHITE, "(0 : メニューを閉じる)");
 	}
 	//なにか買うときのコメント
 	else {
-		DrawStringEx(ARMSDEALERCOMENT.x, ARMSDEALERCOMENT.y, COLOR_WHITE, "「 何を買うんだい 」");
+		DrawStringEx(ARMSDEALERCOMENT.x, ARMSDEALERCOMENT.y, koni::Color::WHITE, "「 何を買うんだい 」");
 	}
 }
 
 //コメントをロードする
-void UIManager::ComentLoad(const int max_num , const std::string& name)
+void UIManager::ComentLoad(const int& max_num , const std::string& name)
 {
 	//一度コメントを初期化する
 	coment_.clear();
@@ -100,34 +84,26 @@ void UIManager::ComentLoad(const int max_num , const std::string& name)
 //コメントを描画する
 void UIManager::ComentDraw(const tnl::Vector2i& coment_pos)
 {
-
-	DrawStringEx(coment_pos.x, coment_pos.y, COLOR_WHITE, "%s", coment_[curent_num].c_str());
+	//コメント描画
+	DrawStringEx(coment_pos.x, coment_pos.y, koni::Color::WHITE, "%s", coment_[curent_num].c_str());
 
 	//閉じるを表示する座標
 	const int ADD_X = 450;  const int ADD_Y = 50;
 
-	DrawStringEx(coment_pos.x + ADD_X, coment_pos.y + ADD_Y, -1, "(0 : 閉じる)");
+	//閉じる文字の描画
+	DrawStringEx(coment_pos.x + ADD_X, coment_pos.y + ADD_Y, koni::Color::WHITE, "(0 : 閉じる)");
 
 }
 
-//整数値を表示できる用のコメント
-//void UIManager::ComentDraw(const tnl::Vector2i& coment_pos, int value)
-//{
-//	DrawStringEx(coment_pos.x, coment_pos.y, COLOR_WHITE, "%s", coment_[curent_num].c_str(), value);
-//
-//	//閉じるを表示する座標
-//	const int ADD_X = 450;  const int ADD_Y = 50;
-//
-//	DrawStringEx(coment_pos.x + ADD_X, coment_pos.y + ADD_Y, -1, "(0 : 閉じる)");
-//}
-
 //コメントを次に移動する
-void UIManager::ComentNextByInput(const int max_draw_num)
+void UIManager::ComentNextByInput(const int& max_draw_num)
 {
 	//描画の最大数を超えてなければ
 	if (count < max_draw_num) {
 		//Enterキーを受け付ける
 		if (tnl::Input::IsKeyDownTrigger(eKeys::KB_RETURN)) {
+			//SEを鳴らす
+			SoundManager::getSoundManager()->sound_Play("sound/SoundEffect/decision.mp3", DX_PLAYTYPE_BACK);
 			//数を増やす
 			curent_num++;
 			//カウントを増やす
@@ -136,56 +112,59 @@ void UIManager::ComentNextByInput(const int max_draw_num)
 	}
 }
 
-//プレイヤーのステータスをHPバーで表示する
+// プレイヤーのステータスをHPバーで表示する
 void UIManager::PlayerStatusDrawWindow()
 {
-	//プレイヤーのステータスを取得する
-	auto& plyerstatus = GameManager::getGameManager()->getPlayer()->getPlayerStatusSave();
+	// プレイヤーアイコンの位置
+	const tnl::Vector2i PLAYER_ICON_POS = { 90 ,120 };
 
-	//プレイヤーの画像
-	ResourceManager::getResourceManager()->DrawRotaGraphEx("graphics/Player/Player_Icon_sentou.png", 90, 120, 1.3f, 0, true);
+	// HPバーの位置、幅、高さ
+	const tnl::Vector2i HP_BAR_POS = { 75 ,155 };
+	const int HP_BAR_WIDTH = 75;
+	const int HP_BAR_HEIGHT = 30;
 
-	//hpバーの画像
+	// MPバーの位置、幅、高さ
+	const tnl::Vector2i MP_BAR_POS = { 75 ,215 };
+	const int MP_BAR_WIDTH = 75;
+	const int MP_BAR_HEIGHT = 30;
+
+	// プレイヤー情報表示の位置
+	const tnl::Vector2i PLAYER_INFO_POS = { 75 ,70 };
+	const tnl::Vector2i LEVEL_INFO_POS = { 120 ,110 };
+	const int HP_INFO_Y = 190;
+	const int MP_INFO_Y = 250;
+
+	// プレイヤーのステータスを取得する
+	auto& playerStatus = GameManager::getGameManager()->getPlayer()->getPlayerStatusSave();
+
+	// プレイヤーの画像を描画
+	ResourceManager::getResourceManager()->DrawRotaGraphEx("graphics/Player/Player_Icon_sentou.png", PLAYER_ICON_POS.x, PLAYER_ICON_POS.y, koni::Numeric::SCALE_ONE_THIRTY_F, 0, true);
+
+	// HPバーの画像をロード
 	int hpber_hdl = ResourceManager::getResourceManager()->LoadGraphEX("graphics/hpbar.png");
-	//mpバーの画像
+	// MPバーの画像をロード
 	int mpber_hdl = ResourceManager::getResourceManager()->LoadGraphEX("graphics/mpbar.png");
-	//hpバー最大の長さ
-	int maxHpBar = 200;
-	//mpバーの最大の長さ
-	int maxMpBer = 200;
 
-	// HPの割合を計算する
-	float hp_ratio = plyerstatus.GetcurentHp() / plyerstatus.GetMaxHp();
-	// HPバーの長さを計算する
-	int hpbar = static_cast<int>(maxHpBar * hp_ratio);
+	// プレイヤーのHPが3割を切った場合、HPバーを赤色に変更
+	if (playerStatus.GetcurentHp() < playerStatus.GetMaxHp() * koni::Numeric::PERCENT_30) {
+		hpber_hdl = ResourceManager::getResourceManager()->LoadGraphEX("graphics/red1_.png");
+	}
 
-	//int hpvar = 0;
+	// HPバーを描画
+	DrawExtendGraph(HP_BAR_POS.x, HP_BAR_POS.y, (HP_BAR_POS.x + static_cast<int>(current_hp_bar)), HP_BAR_POS.y + HP_BAR_HEIGHT, hpber_hdl, true);
+	// MPバーを描画
+	DrawExtendGraph(MP_BAR_POS.x, MP_BAR_POS.y, (MP_BAR_POS.x + static_cast<int>(current_mp_bar)), MP_BAR_POS.y + MP_BAR_HEIGHT, mpber_hdl, true);
 
-	//if (hpbar <= hpbaa) {
-	//	 
-	//	hpbaa -= 2;/*hpバーの減る速度*/
-	//}
-
-
-	// MPの割合を計算する
-	float mp_ratio = plyerstatus.GetCurentMp() / plyerstatus.GetMaxMp();
-
-	// MPバーの長さを計算する
-	int mpbar = static_cast<int>(maxMpBer * mp_ratio);
-
-	DrawExtendGraph(75, 155, (75 + hpbar), 185, hpber_hdl, true);
-	DrawExtendGraph(75, 215, (75 + mpbar), 245, mpber_hdl, true);
-
-	//プレイヤーのHPを表示する
-	DrawStringEx(75, 70, -1, "Player");
-	DrawStringEx(120, 110, -1, " Lv %d", plyerstatus.GetLevel());
-	DrawStringEx(75, 190, -1, " Hp : %d / %d", static_cast<int>(plyerstatus.GetcurentHp()), static_cast<int>(plyerstatus.GetMaxHp()));
-	DrawStringEx(75, 250, -1, " Mp : %d / %d", static_cast<int>(plyerstatus.GetCurentMp()), static_cast<int>(plyerstatus.GetMaxMp()));
-
+	// プレイヤーのHP情報を表示
+	DrawStringEx(PLAYER_INFO_POS.x, PLAYER_INFO_POS.y, koni::Color::WHITE, "Player");
+	DrawStringEx(LEVEL_INFO_POS.x, LEVEL_INFO_POS.y, koni::Color::WHITE, " Lv %d", playerStatus.GetLevel());
+	DrawStringEx(PLAYER_INFO_POS.x, HP_INFO_Y, koni::Color::WHITE, " Hp : %d / %d", playerStatus.GetcurentHp(), playerStatus.GetMaxHp());
+	DrawStringEx(PLAYER_INFO_POS.x, MP_INFO_Y, koni::Color::WHITE, " Mp : %d / %d", playerStatus.GetCurentMp(), playerStatus.GetMaxMp());
 }
 
+
 //セーブした時の文字を表示させる
-void UIManager::SaveText(const tnl::Vector3& text_pos)
+void UIManager::SaveText(const tnl::Vector2i& text_pos)
 {
 	//セーブしている時に
 	if (save_flag) {
@@ -193,7 +172,7 @@ void UIManager::SaveText(const tnl::Vector3& text_pos)
 		//フレームをインクリメント
 		frame++;
 
-		DrawStringEx(text_pos.x, text_pos.y, -1, "セーブ中です。");
+		DrawStringEx(text_pos.x, text_pos.y, koni::Color::WHITE, "セーブ中です。");
 
 		//フレーム数が60を超えたら
 		if (frame >= 60) {
@@ -208,7 +187,7 @@ void UIManager::SaveText(const tnl::Vector3& text_pos)
 	else if (frame >= 60 && !save_flag) {
 
 		//セーブが完了した時のテキストを出す
-		DrawStringEx(text_pos.x, text_pos.y, -1, "セーブしました。");
+		DrawStringEx(text_pos.x, text_pos.y, koni::Color::WHITE , "セーブしました。");
 	}
 
 }
@@ -218,54 +197,175 @@ void UIManager::PlayerMoveDetail(const std::vector<std::string>& detail_text)
 {
 	if (player_detail_window_flag) {
 
+		//メニューウィンドウの座標
+		const tnl::Vector2i DETAIL_WINDOW_POS = { 850, 300 };
+
+		//メニューウィンドウのサイズ
+		const int DETAIL_WINDOW_WIDTH = 400;
+		const int DETAIL_WINDOW_HEIGHT = 400;
+
+		//操作説明文字の座標
+		const tnl::Vector2i MOVE_DETAIL_STRING_POS = { 900, 350 };
+
 		auto menu_window = getMenu("menu_window");
 
-		menu_window->Menu_draw(850, 300, 400, 400);
+		menu_window->Menu_draw(DETAIL_WINDOW_POS.x, DETAIL_WINDOW_POS.y, DETAIL_WINDOW_WIDTH, DETAIL_WINDOW_HEIGHT);
 
 		for (int i = 0; i < detail_text.size(); i++)
 		{
-			DrawStringEx(900, 350 + (i * 50), -1, "%s", detail_text[i].c_str());
+			DrawStringEx(MOVE_DETAIL_STRING_POS.x, MOVE_DETAIL_STRING_POS.y + (i * ADD_OFSET), koni::Color::WHITE, "%s", detail_text[i].c_str());
 		}
 	}
 }
 
-void UIManager::IconAnimation()
+//HPバーやMPバーの計算
+void UIManager::PlayerStatusBarUpdate(const float delta_time)
 {
-	//ポインタを生成する
-	if (icon_animation == nullptr) {
+	// プレイヤーのステータスを取得する
+	auto& playerStatus = GameManager::getGameManager()->getPlayer()->getPlayerStatusSave();
 
-		//プレイヤーの座標
-		auto palyer_pos = GameManager::getGameManager()->getPlayer()->getPlayerPos();
+	// HPバー最大の長さ
+	int maxHpBar = 200;
+	// MPバーの最大の長さ
+	int maxMpBer = 200;
 
-		//カメラの座標
-		auto camera_pos = GameManager::getGameManager()->getCamera()->getTargetPos();
+	// HPの割合を計算する
+	float hp_ratio = static_cast<float>(playerStatus.GetcurentHp()) / playerStatus.GetMaxHp();
+	float newHpBar = maxHpBar * hp_ratio;
 
-		// プレイヤーの描画位置を計算
-		tnl::Vector3 draw_pos = palyer_pos - camera_pos + tnl::Vector3(DXE_WINDOW_WIDTH >> 1, DXE_WINDOW_HEIGHT >> 1, 0);
-
-		//アイコン用のアニメーション用のポインタを生成する
-		icon_animation = std::make_shared<Animation>("graphics/talk_icon.png", draw_pos.x, draw_pos.y - 80, 3, 1, 32, 32, 3, 8);
-
-		//描画を行う
-		icon_animation->play_animation();
+	// HPバーの補間を更新
+	if (std::fabs(newHpBar - hp_end) > FLT_EPSILON) {
+		hp_start = current_hp_bar;
+		hp_end = newHpBar;
+		hp_lerp_time = 0.0f;
 	}
-	//ポインタが生成場合
+
+	hp_lerp_time += delta_time;
+
+	hp_lerp_time = (hp_lerp_time > LERPDURATION) ? LERPDURATION : hp_lerp_time;
+
+	current_hp_bar = tnl::SmoothLerp(hp_start, hp_end, LERPDURATION, hp_lerp_time, 0);
+
+	// MPの割合を計算する
+	float mp_ratio = static_cast<float>(playerStatus.GetCurentMp()) / playerStatus.GetMaxMp();
+	float newMpBar = maxMpBer * mp_ratio;
+
+	// MPバーの補間を更新
+	if (std::fabs(newMpBar - mp_end) > FLT_EPSILON) {
+		mp_start = current_mp_bar;
+		mp_end = newMpBar;
+		mp_lerp_time = 0.0f;
+	}
+	mp_lerp_time += delta_time; 
+
+	mp_lerp_time = (mp_lerp_time > LERPDURATION) ? LERPDURATION : mp_lerp_time;
+
+	current_mp_bar = tnl::SmoothLerp(mp_start, mp_end, LERPDURATION, mp_lerp_time, 0);
+}
+
+
+bool UIManager::StoryDisplayUpdate(const float delta_time)
+{
+	story_display_timer += delta_time;
+
+	// タイマーが間隔を超えたら次のコメントに追加
+	if (story_display_timer > koni::Numeric::SECONDS_1_AND_HALF) {
+		story_display_timer = 0.0f; // タイマーをリセット
+
+		// ストーリーが空でない場合、次のコメントを追加
+		if (current_story_comment_index < story_.size()) {
+			displayed_story_coments.push_back(story_[current_story_comment_index]);
+			current_story_comment_index++;
+		}
+
+	}
 	else {
 
-		//プレイヤーの座標
-		auto palyer_pos = GameManager::getGameManager()->getPlayer()->getPlayerPos();
-
-		//カメラの座標
-		auto camera_pos = GameManager::getGameManager()->getCamera()->getTargetPos();
-
-		// プレイヤーの描画位置を計算
-		tnl::Vector3 draw_pos = palyer_pos - camera_pos + tnl::Vector3(DXE_WINDOW_WIDTH >> 1, DXE_WINDOW_HEIGHT >> 1, 0);
-
-		//アイコンの座標を切り替える
-		icon_animation->SetAnimationPos(draw_pos.x , draw_pos.y - 50);
-
-		//描画を行う
-		icon_animation->play_animation();
+		// コメントが全て表示されたらtrueを返す
+		if (current_story_comment_index >= story_.size()) {
+			
+			return true;
+		}
 	}
 
+	return false;
 }
+
+void UIManager::StoryDisplay(const int& font_color)
+{
+	// 初期Y座標
+	int y_offset = 80;
+	//X座標
+	const int STORY_COMENT_X = 100;
+
+    for (const auto& comment : displayed_story_coments) {
+        DrawStringEx(STORY_COMENT_X, y_offset, font_color, "%s", comment.c_str());
+        y_offset += ADD_OFSET; // 各コメントの表示位置をずらす
+    }
+}
+
+void UIManager::StoryLoad(const int& section_type)
+{
+	//一度コメントを初期化する
+	displayed_story_coments.clear();
+	current_story_comment_index = 0;
+	story_.clear();
+
+	auto story_csv = CsvManager::getCsvManager()->GetStoryCsv();
+
+	for (int i = 1; i < story_csv.size(); i++) {
+
+		//どのタイプなのかを確認してストーリーを格納する
+		if (section_type == std::stoi(story_csv[i][0].c_str())) {
+
+			story_.emplace_back(story_csv[i][1]);
+		}
+	}
+}
+
+UIManager::~UIManager()
+{
+	//中のメニューを解放する
+	menu_map.clear();
+}
+
+//void UIManager::IconAnimation()
+//{
+//	//ポインタを生成する
+//	if (icon_animation == nullptr) {
+//
+//		//プレイヤーの座標
+//		auto palyer_pos = GameManager::getGameManager()->getPlayer()->getPlayerPos();
+//
+//		//カメラの座標
+//		auto camera_pos = GameManager::getGameManager()->getCamera()->getTargetPos();
+//
+//		// プレイヤーの描画位置を計算
+//		tnl::Vector3 draw_pos = palyer_pos - camera_pos + tnl::Vector3(DXE_WINDOW_WIDTH >> 1, DXE_WINDOW_HEIGHT >> 1, 0);
+//
+//		//アイコン用のアニメーション用のポインタを生成する
+//		icon_animation = std::make_shared<Animation>("graphics/talk_icon.png", draw_pos.x, draw_pos.y - 80, 3, 1, 32, 32, 3, 8);
+//
+//		//描画を行う
+//		icon_animation->play_animation();
+//	}
+//	//ポインタが生成場合
+//	else {
+//
+//		//プレイヤーの座標
+//		auto palyer_pos = GameManager::getGameManager()->getPlayer()->getPlayerPos();
+//
+//		//カメラの座標
+//		auto camera_pos = GameManager::getGameManager()->getCamera()->getTargetPos();
+//
+//		// プレイヤーの描画位置を計算
+//		tnl::Vector3 draw_pos = palyer_pos - camera_pos + tnl::Vector3(DXE_WINDOW_WIDTH >> 1, DXE_WINDOW_HEIGHT >> 1, 0);
+//
+//		//アイコンの座標を切り替える
+//		icon_animation->SetAnimationPos(draw_pos.x , draw_pos.y - 50);
+//
+//		//描画を行う
+//		icon_animation->play_animation();
+//	}
+//
+//}
--- ManagedDxlGame2023f/ManagedDxlGame/program/game/Manager/UiManager.h
+++ ManagedDxlGame2023f/ManagedDxlGame/program/game/Manager/UiManager.h
@@ -6,6 +6,7 @@
 //-------------------------------------------------------------------------------------------------------------
 
 #include "../../dxlib_ext/dxlib_ext.h"
+#include"../../koni_name_space/common/common_value.h"
 #include"../Menu/MenuWindow.h"
 #include <unordered_map>
 
@@ -19,14 +20,10 @@ public:
 	static UIManager* getUIManager();
 	static void delategetUIManager() { delete getUIManager(); }
 
-	//更新処理
-	void Update(float delta_time);
-
-	//描画
-	void Draw();
-
-	//メニューを追加する
-	void addMenu(const std::string& menuName, std::shared_ptr<Menu> menu);
+	//メニューを追加する(主に他の所でウィンドを使い回したい場合に使用)
+	//arg_ : メニューの名前
+	//arg_ : 追加したいMenuクラスのSharedポインタ
+	void addMenu(const std::string& menuName, const std::shared_ptr<Menu>& menu);
 
 	// メニューを取得する
 	std::shared_ptr<Menu> getMenu(const std::string& menuName) {
@@ -41,21 +38,27 @@ public:
 	}
 
 	//メニューを描画する
+	//arg_1 : キーの名前
+	//arg_2 : X座標
+	//arg_3 : Y座標
+	//arg_4 : Menuの幅のサイズ
+	//arg_5 : Menuの縦のサイズ
 	void Menu_Draw(const std::string& menuName, const int& menu_x, const int& menu_y, const int& menu_width, const int& menu_height);
 
 	//武器屋のコメント
-	void armsdealerComentDraw(int type);
+	void armsdealerComentDraw(const int& type);
 
 	//コメントをロードする
-	void ComentLoad(const int max_num ,const std::string& name);
+	//arg_1 : コメントの数
+	//arg_2 : 名前(NPCなどの名前を照らし合わせてそれに該当したコメントをロードする)
+	void ComentLoad(const int& max_num ,const std::string& name);
 
 	//コメントを表示する
 	void ComentDraw(const tnl::Vector2i& coment_pos);
 
-	//void ComentDraw(const tnl::Vector2i& coment_pos , int value);
-
 	//コメントを次に移動する
-	void ComentNextByInput(const int max_draw_num);
+	//arg_1 : コメントの描画数
+	void ComentNextByInput(const int& max_draw_num);
 
 	//コメントを初期化して１から表示させる
 	void ComentClear() { curent_num = 0; }
@@ -73,7 +76,7 @@ public:
 	void PlayerStatusDrawWindow();
 
 	//セーブした時の文字を表示させる
-	void SaveText(const tnl::Vector3& text_pos );
+	void SaveText(const tnl::Vector2i& text_pos );
 
 	//セーブテキストの表示を切り替える（セーブテキストを表示する）
 	void SaveTextFlagChange() { save_flag = !save_flag; }
@@ -86,32 +89,51 @@ public:
 		player_detail_window_flag = !player_detail_window_flag;
 	}
 
+	//操作説明の表示を管理するフラグを取得する
+	const bool GetPlayerDetailFlage()const {
+		return player_detail_window_flag;
+	}
+
 	//操作説明のテキストを取得する
-	std::vector<std::string>&GetPlayerMoveDetailText() {
+	const std::vector<std::string>& GetPlayerMoveDetailText() const {
 		return operation_instructions;
 	}
 
 	//バトル用の説明のテキストを取得する
-	std::vector<std::string>&GetBattlePlayerMoveDetailText() {
+	const std::vector<std::string>&GetBattlePlayerMoveDetailText() const {
 		return battle_operation_instructions;
 	}
 
-	//話かける際のアイコンの表示
-	void IconAnimation();
+	//HPバーやMPバーの計算
+	void PlayerStatusBarUpdate(const float delta_time);
 
-	//アイコン用のアニメーションを止める
-	void IconAnimationStop() { if (icon_animation) { icon_animation->stop_animation();  } }
+	//ストーリーの文字をふやしていく
+	//流しきったらtrueを返す
+	bool StoryDisplayUpdate(const float delta_time);
+
+	//ストーリーの文字を表示する
+	void StoryDisplay(const int& font_color);
+
+	//ストーリーの文字のロード
+	void StoryLoad(const int& section_type);
 
 private:
 
-	UIManager(){}
-	~UIManager() {};
+
+	UIManager() = default;
+	~UIManager();
 
 	//メニューを管理するマップ
 	std::unordered_map<std::string, Shared<Menu>>menu_map;
 
 	//コメントを格納する配列
 	std::vector<std::string>coment_;
+
+	//ストリー用の配列
+	std::vector<std::string>story_;
+
+	//ストーリー用のカウント
+	int curent_story = 0;
 
 	//---それぞれのコメント---//
 
@@ -130,9 +152,6 @@ private:
 	//武器屋のメニューを閉じるコメントの座標
 	const tnl::Vector2i MENU_CLOSE = { 70, 150 };
 
-	//白色
-	const int COLOR_WHITE = -1;
-
 	//現在のコメント
 	int curent_num = 0;
 
@@ -148,8 +167,44 @@ private:
 	//説明表示を切り替える為のフラグ
 	bool player_detail_window_flag = true;
 
-	//アイコンのアニメーション用のポインタ
-	Shared<Animation>icon_animation = nullptr;
+	// HPバーの現在の長さを保持する変数
+	float current_hp_bar = 0;
+
+	// MPバーの現在の長さを保持する変数
+	float current_mp_bar = 0;
+
+	// HPバーの補間に使用する経過時間を保持する変数
+	float hp_lerp_time = 0;
+
+	// MPバーの補間に使用する経過時間を保持する変数
+	float mp_lerp_time = 0;
+
+	// 補間の総時間（秒）を保持する定数
+	const float LERPDURATION = 1.0f;
+
+	// 補間の開始時点でのHPバーの長さを保持する変数
+	float hp_start = 0;
+
+	// 補間の終了時点でのHPバーの長さを保持する変数
+	float hp_end = 0;
+
+	// 補間の開始時点でのMPバーの長さを保持する変数
+	float mp_start = 0;
+
+	// 補間の終了時点でのMPバーの長さを保持する変数
+	float mp_end = 0;
+
+	// 現在表示しているコメントのインデックス
+	int current_story_comment_index = 0; 
+	
+	// 現在表示中のコメントのリスト
+	std::vector<std::string> displayed_story_coments;
+	
+	// コメント表示のタイマー
+	float story_display_timer = 0.0f; 
+
+	//追加座標
+	const int ADD_OFSET = 50;
 
 //--------------------------------------------------------------------------------------------------------------
 //UIの説明文用の文字列
