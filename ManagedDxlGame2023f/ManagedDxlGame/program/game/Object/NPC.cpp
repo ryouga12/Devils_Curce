c50a09fdd894880bb660490263898b8a47d9bf54
ryouga12 <ryouga.20011108@gmail.com>
2024-09-20 16:19:26 +0900

    [追加]NPCクラスの画像ファイルをcsvを使って読み込む関数及びコメントの追加

--- ManagedDxlGame2023f/ManagedDxlGame/program/game/Object/NPC.cpp
+++ ManagedDxlGame2023f/ManagedDxlGame/program/game/Object/NPC.cpp
@@ -2,24 +2,26 @@
 #include"../Manager/ResourceManager.h"
 #include"../Manager/GameManager.h"
 #include"../Manager/UiManager.h"
+#include"../Manager/CsvManager.h"
 
 
-//Npcのコンストラクタ : 引数 : ( 名前 , 座標 ,キャラのサイズ , ハンドルのPass , 画像の大きさ , どのアニメーションから始めるか, ハンドルの数 , アイコン )
-Npc::Npc(const std::string& name, const tnl::Vector3& pos, const int CHARASIZE, std::string ghdl_pass[], float scale , const NPC_ANIMATION animation_namber , const int ghdl_num , const std::string& npc_icon_image)
+//Npcのコンストラクタ 
+Npc::Npc(const std::string& name, const tnl::Vector3& pos,const int& CHIP_SIZE, const float& scale , const NPC_ANIMATION& animation_namber , const std::string& npc_icon_image)
 {
 	//引数を代入して呼び出し元で使う
 	npc_name_ = name;
 	npc_pos_ = pos;
 	scale_ = scale;
-	npc_chara_chip.y = CHARASIZE;
-	npc_chara_chip.x = CHARASIZE;
+	npc_chara_chip.x = CHIP_SIZE;
+	npc_chara_chip.y = CHIP_SIZE;
 	anim_ctrl_dir = animation_namber;
-	npc_icon_hdl = ResourceManager::getResourceManager()->LoadGraphEX(npc_icon_image.c_str());
-	
-	for (int i = 0; i < ghdl_num; i++)
-	{
-		LoadDivGraph(ghdl_pass[i].c_str(), 3, 3, 1, npc_chara_chip.x, npc_chara_chip.y, anim_hdls[i]);
-	}
+	npc_icon_hdl = ResourceManager::GetResourceManager()->LoadGraphEX(npc_icon_image.c_str());
+}
+
+Npc::Npc(const std::string& name, const std::string& icon_hdl)
+{
+	npc_name_ = name;
+	npc_icon_hdl = ResourceManager::GetResourceManager()->LoadGraphEX(icon_hdl.c_str());
 }
 
 Npc::~Npc()
@@ -29,9 +31,13 @@ Npc::~Npc()
 		for (int k = 0; k < 5; k++) {
 			DeleteGraph(anim_hdls[i][k]);
 			// 解放後のポインタの値をデバッグトレースで確認
-			tnl::DebugTrace("Deleted Npcimage at [%d][%d]. Pointer value: %p\n", i, k, anim_hdls[i][k]);
+			tnl::DebugTrace("\nDeleted Npcimage at [%d][%d]. Pointer value: %p\n", i, k, anim_hdls[i][k]);
 		}
 	}
+
+	tnl::DebugTrace("\n------------------------------------------------------------");
+	tnl::DebugTrace("\n %sが解放されました" , npc_name_.c_str());
+	tnl::DebugTrace("\n------------------------------------------------------------");
 
 }
 
@@ -52,12 +58,41 @@ void Npc::Update(float delta_time)
 //描画
 void Npc::Draw(const KonCamera& camera)
 {
-	tnl::Vector3 draw_pos = npc_pos_ - camera.getTargetPos() + tnl::Vector3(DXE_WINDOW_WIDTH >> 1, DXE_WINDOW_HEIGHT >> 1, 0);
-	DrawRotaGraph(static_cast<int>(draw_pos.x), static_cast<int>(draw_pos.y), scale_, COLOR_BRACK, anim_hdls[static_cast<int>(anim_ctrl_dir)][anim_ctrl_frame], true);
+	tnl::Vector3 draw_pos = npc_pos_ - camera.GetTargetPos() + tnl::Vector3(DXE_WINDOW_WIDTH >> 1, DXE_WINDOW_HEIGHT >> 1, 0);
+	DrawRotaGraph(static_cast<int>(draw_pos.x), static_cast<int>(draw_pos.y), scale_, koni::Color::BLACK, anim_hdls[static_cast<int>(anim_ctrl_dir)][anim_ctrl_frame], true);
 }
 
-//---------------------------------------------------------------------------------------------------------
-//セーブを行うNPC
+//Npcの画像を格納する
+void Npc::SetNpcImage(const std::string& npc_name, const int& inmap_state)
+{
+	//アニメーション画像を格納配列
+	std::vector<std::string> npc_image;
+
+	auto npc_image_size = CsvManager::GetCsvManager()->GetNpcImageCsv().size();
+
+	for (int i = 1; i < npc_image_size; i++) {
+
+		//npcの名前とどこのマップに今いるかによってその時の画像のロードを行う
+		if (npc_name == CsvManager::GetCsvManager()->GetNpcImageCsv()[i][0].c_str() &&  inmap_state == std::stoi(CsvManager::GetCsvManager()->GetNpcImageCsv()[i][1].c_str())) {
+			//2列目までは名前とシーンの数値が入っている為＋2して3列目から数える
+			for (int k = 2; k < static_cast<int>(NPC_ANIMATION::NPC_DIR_MAX) + 2; k++) {
+				npc_image.emplace_back(CsvManager::GetCsvManager()->GetNpcImageCsv()[i][k]);
+			}
+		}
+	}
+
+	for (int i = 0; i < static_cast<int>(NPC_ANIMATION::NPC_DIR_MAX); i++) {
+
+		LoadDivGraph(
+			npc_image[i].c_str(),							// 画像のパス
+			TOTAL_FRAME,									// 総フレーム数
+			HORIZONTAL_FRAME,								// 横フレーム数
+			VERTICAL_FRAME,									// 縦フレーム数
+			npc_chara_chip.x,								// 横方向へ 1フレームあたりの幅
+			npc_chara_chip.y,								// 縦方向へ 1フレームあたりの幅
+			anim_hdls[i]);									// 画像ハンドルを格納する配列名);
+	}
+}
 
 
 
--- ManagedDxlGame2023f/ManagedDxlGame/program/game/Object/NPC.h
+++ ManagedDxlGame2023f/ManagedDxlGame/program/game/Object/NPC.h
@@ -1,7 +1,9 @@
 #pragma once
-///
-///NPCクラス
-///
+//------------------------------------------------------------------------------------------------------------
+//
+//NPCクラス
+//
+//-------------------------------------------------------------------------------------------------------------
 
 
 #include "../../dxlib_ext/dxlib_ext.h"
@@ -11,11 +13,8 @@
 #include<unordered_map>
 
 class MenuWindow;
-class GameManager;
-class UIManager;
 
-
-class Npc  : public Actor{
+class Npc  final : public Actor{
 public:
 
 	//NPCのアニメーション
@@ -32,27 +31,38 @@ public:
 		NPC_DIR_MAX
 	};
 
-	//Npcのコンストラクタ : 引数 : ( 名前 , 座標 , キャラのサイズ ,ハンドルのPass , 画像の大きさ , どのアニメーションから始めるか, ハンドルの数 )
-	Npc(const std::string& name, const tnl::Vector3& pos ,const int CHARASIZE, std::string ghdl_pass[],float scale , const NPC_ANIMATION animation_namber , const int ghdl_num, const std::string& npc_icon_image);
+	//Npcのコンストラクタ 
+	//arg_1  : 名前 
+	//arg_2  : 座標 
+	//arg_ 3 : キャラのサイズ 
+	//arg_4  : 画像の大きさ 
+	//arg_ 5 : どのアニメーションから始めるか
+	//arg_ 6 : npcのアイコン )
+	Npc(const std::string& name, const tnl::Vector3& pos ,const int& CHARASIZE, const float& scale , const NPC_ANIMATION& animation_namber , const std::string& npc_icon_image);
+	
+	//当たり判定のないNpc
+	//arg_1 : 名前
+	//arg_2 : アイコンのハンドル 
+	Npc(const std::string& name , const std::string& icon_hdl);
+		
 	~Npc();
 
 	//更新処理
-	virtual void Update(float delta_time)override;
+	void Update(float delta_time)override;
 
 	//描画
-	virtual void Draw(const KonCamera& camera)override;
+	void Draw(const KonCamera& camera)override;
 
 	//Npcのキャラチップの幅と高さを取得する
 	int GetNpcSizeWidth()const {
 		return npc_chara_chip.x;
 	}
-
 	int GetNpcSizeHegiht()const {
 		return npc_chara_chip.y;
 	}
 
 	//Npcのキャラのサイズを取得する
-	tnl::Vector2i GetNpcCharaChip()const {
+	const tnl::Vector2i& GetNpcCharaChip()const {
 		return npc_chara_chip;
 	}
 
@@ -69,20 +79,15 @@ public:
 		return anim_ctrl_dir;
 	}
 
-	//保管しているアニメーションをを取得する
-	NPC_ANIMATION GetNpcAnimation_Before()const {
-		return before_animation_save;
-	}
-
-	//変更する前のアニメーションを保管する
-	void SetNpcAnimationSave(NPC_ANIMATION save_animation) {
-		before_animation_save = save_animation;
-	}
-
 	//Npcのアイコンを取得する
 	int GetNpcIconImage()const {
 		return npc_icon_hdl;
 	}
+
+	//Npcの画像をロードする
+	//arg_1 : Npcの名前,InMapScene内の現在のシーン
+	//名前と現在のシーンを両方比較して該当した画像を格納する
+	void SetNpcImage(const std::string& npc_name , const int& inmap_state);
 
 //------------------------------------------------------------------------------------------------------------------------
 //アニメーション
@@ -95,44 +100,16 @@ private:
 	//アニメーションの選択
 	NPC_ANIMATION anim_ctrl_dir = NPC_ANIMATION::NPC_DIR_DOWN;
 
-	//一個前のアニメーションを保存しておく為の変数
-	//主にアニメーションを戻すために使用
-	NPC_ANIMATION before_animation_save;
-
 	//フレーム
 	int anim_ctrl_frame = 0;						
 
 	//アニメーションの配列
 	int anim_hdls[static_cast<int>(NPC_ANIMATION::NPC_DIR_MAX)][5];
 
-
 public:
 
 	//アニメーションをセットする
 	void SetNpcAnimation(NPC_ANIMATION new_animation) { anim_ctrl_dir = new_animation; }
-
-
-//------------------------------------------------------------------------------------------------------------------------
-//メニュー関係
-
-private:
-
-	//武器屋の話しかけたときのコメント
-	int armsdealer_first_comet = 0;
-
-	//武器屋の文字の座標
-	tnl::Vector2i weaponShop = { 60 , 520 };
-
-	//武器商人のコメントの座標
-	tnl::Vector2i armsdealerComent = { 150, 550 };
-
-	//武器屋の購入コメントの座標
-	tnl::Vector2i WeaponShop_buy = { 70, 100 };
-
-	//武器屋のメニューを閉じるコメントの座標
-	tnl::Vector2i menu_close = { 70, 150 };
-
-
 
 //------------------------------------------------------------------------------------------------------------------------
 //Npc関係
